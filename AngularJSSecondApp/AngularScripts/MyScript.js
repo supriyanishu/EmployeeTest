773897787ba180552e9cbcf66ffe560e176f36f9
Supriya Singh <supriya.nishu@gmail.com>
2020-09-18 10:54:41 -0700

    second checin

--- AngularJSSecondApp/AngularScripts/MyScript.js
+++ AngularJSSecondApp/AngularScripts/MyScript.js
@@ -1,13 +1,13 @@
-﻿//var app = angular.module("myShoppingList", []);
-//app.controller("myCtrl", function($scope) {
-//  $scope.products = ["Milk", "Bread", "Cheese"];
-//  $scope.addItem = function () {
-//    $scope.products.push($scope.addMe);
-//  }
-//  $scope.removeItem = function (x) {
-//    $scope.products.splice(x, 1);
-//  }
-//});
+﻿var app = angular.module("myShoppingList", []);
+app.controller("myCtrl", function($scope) {
+  $scope.products = ["Milk", "Bread", "Cheese"];
+  $scope.addItem = function () {
+    $scope.products.push($scope.addMe);
+  }
+  $scope.removeItem = function (x) {
+    $scope.products.splice(x, 1);
+  }
+});
 
 var app = angular
     .module("myModule", [])
